699582f6a6dc0bec58548196ed490a0833ba3558
Iuri <iuriguilhermesantos@gmail.com>
2025-10-23 13:12:52 -0300

    add: comeco do controller do candidato

--- src/main/java/br/csi/oportunidades/controller/candidato/CandidatoController.java
+++ src/main/java/br/csi/oportunidades/controller/candidato/CandidatoController.java
@@ -1,6 +1,7 @@
 package br.csi.oportunidades.controller.candidato;
 
 
+import br.csi.oportunidades.model.Inscricao;
 import br.csi.oportunidades.service.candidato.CandidatoService;
 import br.csi.oportunidades.util.UsuarioAutenticado;
 import lombok.AllArgsConstructor;
@@ -13,6 +14,13 @@ public class CandidatoController {
 
     private final CandidatoService candidatoService;
     private final UsuarioAutenticado usuarioAutenticado;
+
+
+
+    @PostMapping("/me/inscrever/{id}")
+    public void inscrever(@RequestBody Inscricao i, @PathVariable Long id) {
+        candidatoService.inscrever(i);
+    }
 
 
 }
